f3db61e8c3adf6ce2e797749a67449ee870f4116
Vladislav <plotnikov12341@gmail.com>
2026-07-16 20:14:20 +0300

    chore: register auth interceptors and providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,20 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { CocktailRepository } from './features/cocktails/cocktail.repository';
 import { CocktailApiService } from './features/cocktails/cocktail-api.service';
+import { attachTokenInterceptor } from './core/auth/interceptors/attach-token.interceptor';
+import { refreshOnUnauthorizedInterceptor } from './core/auth/interceptors/refresh-on-unauthorized.interceptor';
+import { AuthRepository } from './core/auth/auth.repository';
+import { AuthApiService } from './core/auth/services/auth-api.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([attachTokenInterceptor, refreshOnUnauthorizedInterceptor])),
+    { provide: AuthRepository, useClass: AuthApiService },
     { provide: CocktailRepository, useClass: CocktailApiService },
   ],
 };
