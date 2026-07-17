5a57b7ef38a65886df8bc1490e654c3bdf08c2df
Vladislav <plotnikov12341@gmail.com>
2026-07-17 19:20:28 +0300

    chore: set up routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/auth/auth-shell/auth-shell.component').then((m) => m.AuthShellComponent),
+    children: [
+      {
+        path: 'register',
+        title: 'Регистрация — Барная карта',
+        loadComponent: () =>
+          import('./features/auth/register/register.component').then((m) => m.RegisterComponent),
+      },
+      {
+        path: 'login',
+        title: 'Вход — Барная карта',
+        loadComponent: () =>
+          import('./features/auth/login/login.component').then((m) => m.LoginComponent),
+      },
+    ],
+  },
+];
