41868ab7010e69f9ea17e232c08af91cb0ffe762
Vladislav <plotnikov12341@gmail.com>
2026-07-20 13:42:35 +0300

    chore: update auth guard path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './core/auth/auth.guard';
+import { authGuard } from './core/auth/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'cocktails' },
